83e63d31b9fe55c23f4d8fa18c84f762e18f590e
Steven Spencer <steve35spencer@gmail.com>
2020-03-19 17:19:52 -0400

    cleaned up code

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import styled from "styled-components";
 import { Spinner } from 'reactstrap';
-import { Collapse, Button, Card, CardBody } from "reactstrap";
+import { Collapse } from "reactstrap";
 
 const Image = props => {
 
